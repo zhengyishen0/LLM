ef2a34c38d058facaec2ce51c93a1969d794ccef
Zhengyi Shen <zhengyishen1@gmail.com>
2024-02-28 18:47:42 -0500

    update openai whisper api

--- modules/openai_whisper.py
+++ modules/openai_whisper.py
@@ -1,12 +1,56 @@
-from openai import OpenAI
+from __future__ import annotations
+
+import os
+from io import BytesIO
+
+from speech_recognition.audio import AudioData
+from speech_recognition.exceptions import SetupError
 
 
-def transcription(audio=None, audio_file=None):
+def openai_whisper(
+    audio_data: "AudioData",
+    *,
+    model: str = "whisper-1",
+    api_key: str | None = None,
+):
+    """
+    Performs speech recognition on ``audio_data`` (an ``AudioData`` instance), using the OpenAI Whisper API.
+
+    This function requires an OpenAI account; visit https://platform.openai.com/signup, then generate API Key in `User settings <https://platform.openai.com/account/api-keys>`__.
+
+    Detail: https://platform.openai.com/docs/guides/speech-to-text
+
+    Raises a ``speech_recognition.exceptions.SetupError`` exception if there are any issues with the openai installation, or the environment variable is missing.
+    """
+    if not isinstance(audio_data, AudioData):
+        raise ValueError("``audio_data`` must be an ``AudioData`` instance")
+    if api_key is None and os.environ.get("OPENAI_API_KEY") is None:
+        raise SetupError("Set environment variable ``OPENAI_API_KEY``")
+
+    try:
+        from openai import OpenAI
+    except ImportError:
+        raise SetupError(
+            "missing openai module: ensure that openai is set up correctly."
+        )
     client = OpenAI()
 
-    audio = audio or open(audio_file, "rb")
-    transcription = client.audio.transcriptions.create(
-        model="whisper-1",
-        file=audio
+    wav_data = BytesIO(audio_data.get_wav_data())
+    wav_data.name = "SpeechRecognition_audio.wav"
+
+    transcript = client.audio.transcriptions.create(
+        model=model,
+        file=wav_data
     )
-    return transcription.text
+    return transcript.text
+
+
+# def openai_whisper(audio=None, audio_file=None):
+#     client = OpenAI()
+
+#     audio = audio or open(audio_file, "rb")
+#     transcription = client.audio.transcriptions.create(
+#         model="whisper-1",
+#         file=audio
+#     )
+#     return transcription.text
--- voice_recognition.py
+++ voice_recognition.py
@@ -1,52 +1,25 @@
 #!/usr/bin/env python3
 # %%
-import time
+from modules.openai_whisper import openai_whisper
 import speech_recognition as sr
 
 
-# this is called from the background thread
-def callback(recognizer, audio):
-    # received audio data, now we'll recognize it using Google Speech Recognition
-    try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        print("Google Speech Recognition thinks you said " +
-              recognizer.recognize_google(audio))
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print(
-            "Could not request results from Google Speech Recognition service; {0}".format(e))
+# obtain path to "english.wav" in the same folder as this script
+AUDIO_FILE = "data/audio.wav"
 
-
-# %%
-# obtain audio from the microphone
+# use the audio file as the audio source
 r = sr.Recognizer()
-m = sr.Microphone()
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source)  # read the entire audio file
 
-with m as source:
-    # we only need to calibrate once, before we start listening
-    r.adjust_for_ambient_noise(source)
-
-# %%
-# start listening in the background (note that we don't have to do this inside a `with` statement)
-stop_listening = r.listen_in_background(m, callback)
-# `stop_listening` is now a function that, when called, stops background listening
 
 # %%
 
-# do some unrelated computations for 5 seconds
-for _ in range(50):
-    # we're still listening even though the main thread is doing other things
-    time.sleep(0.1)
+try:
+    print(
+        f"Whisper thinks you said {openai_whisper(audio)}")
+except sr.RequestError as e:
+    print("Could not request results from Whisper API")
 
-# calling this function requests that the background listener stop listening
-stop_listening(wait_for_stop=False)
-
-# do some more unrelated things
-while True:
-    # we're not listening anymore, even though the background thread might still be running for a second or two while cleaning up and stopping
-    time.sleep(0.1)
 
 # %%
